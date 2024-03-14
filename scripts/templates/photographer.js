d4b7069a7193c3e4824901c3bfedb12b2f2edc9d
Damien <damien.dev2022@gmail.com>
2024-03-14 16:04:34 +0100

    étapes 3 intégrer la page d'accueil

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,5 @@
 export function photographerTemplate(data) {
-  const { name, portrait } = data;
+  const { name, portrait, country, city, tagline, price } = data;
 
   const picture = `assets/photographers/Portrait/${portrait}`;
 
@@ -8,9 +8,22 @@ export function photographerTemplate(data) {
     const img = document.createElement("img");
     img.setAttribute("src", picture);
     const h2 = document.createElement("h2");
+    const countryElement = document.createElement("p");
+    const taglineElement = document.createElement("p");
+    const priceElement = document.createElement("p");
+
     h2.textContent = name;
+    h2.classList.add("namePhotographer");
+    countryElement.textContent = city + ", " + country;
+    countryElement.classList.add("countryPhotographer");
+    taglineElement.textContent = tagline;
+    priceElement.textContent = price + "€ /jour";
+    /***On rattache l'élément crée a un parent */
     article.appendChild(img);
     article.appendChild(h2);
+    article.appendChild(countryElement);
+    article.appendChild(taglineElement);
+    article.appendChild(priceElement);
     return article;
   }
   return { name, picture, getUserCardDOM };
