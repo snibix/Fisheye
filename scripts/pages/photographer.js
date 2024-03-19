6d27fd174df24759c27360b5f134c005a79f903e
Damien <damien.dev2022@gmail.com>
2024-03-19 13:40:21 +0100

    avancement etape 4 recuperation de l'id photographers pour la page photographers

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,4 @@
 //Mettre le code JavaScript lié à la page photographer.html
+let params = new URLSearchParams(window.location.search);
+let idPhotographer = params.get("id");
+console.log(idPhotographer);
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,26 +1,35 @@
 export function photographerTemplate(data) {
-  const { name, portrait, country, city, tagline, price } = data;
+  const { name, portrait, country, city, tagline, price, id } = data;
 
   const picture = `assets/photographers/Portrait/${portrait}`;
 
   function getUserCardDOM() {
     const article = document.createElement("article");
-    const img = document.createElement("img");
-    img.setAttribute("src", picture);
-    const h2 = document.createElement("h2");
-    const countryElement = document.createElement("p");
-    const taglineElement = document.createElement("p");
-    const priceElement = document.createElement("p");
 
+    const img = document.createElement("img");
+    img.src = picture;
+
+    const h2 = document.createElement("h2");
     h2.textContent = name;
-    h2.classList.add("namePhotographer");
+    // h2.className = "name_photographer";
+
+    const countryElement = document.createElement("p");
     countryElement.textContent = city + ", " + country;
-    countryElement.classList.add("countryPhotographer");
+    countryElement.className = "country_photographer";
+
+    const taglineElement = document.createElement("p");
     taglineElement.textContent = tagline;
+
+    const priceElement = document.createElement("p");
     priceElement.textContent = price + "€ /jour";
+
+    const url = document.createElement("a");
+    url.href = `./photographer.html?id=${id}`;
+
     /***On rattache l'élément crée a un parent */
-    article.appendChild(img);
-    article.appendChild(h2);
+    url.appendChild(img);
+    url.appendChild(h2);
+    article.appendChild(url);
     article.appendChild(countryElement);
     article.appendChild(taglineElement);
     article.appendChild(priceElement);
