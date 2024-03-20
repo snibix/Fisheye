f99cc8dcd8cec12bca01a40a9d7ca0dbb7e7e218
Damien <damien.dev2022@gmail.com>
2024-03-20 16:24:38 +0100

    etapes 5 : ajout de la page photographe static

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,4 +1,5 @@
-import { getPhotographer } from "../utils/data.js";
+import { photographerTemplate } from "../templates/photographerCard.js";
+import { getPhotographer, getPhotographers } from "../utils/data.js";
 
 //Mettre le code JavaScript lié à la page photographer.html
 let params = new URLSearchParams(window.location.search);
@@ -6,3 +7,20 @@ let photographerId = parseInt(params.get("id"));
 console.log(photographerId);
 
 console.log(await getPhotographer(photographerId));
+
+function cardMediaPhotographer(photographerId, data) {
+  const dataPhotographer = getPhotographer(photographerId);
+  const { name, portrait, country, city, tagline, price, id } = data;
+  function cardPhotographer(data, dataPhotographer) {
+    const article = document.createElement("artcile");
+
+    const img = document.createElement("img");
+
+    const namePhotographer = document.createElement("h1");
+    namePhotographer.textContent = name;
+
+    article.appendChild(namePhotographer);
+    return article;
+  }
+}
+console.log(cardMediaPhotographer(photographerId, data));
