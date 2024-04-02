b109f087f4857f3e43145e6c2e4c245072411d21
Damien <damien.dev2022@gmail.com>
2024-04-02 15:37:43 +0200

    étapes 6 : affichage de la modal , etapes 6 finit

--- scripts/templates/mediaFactory.js
+++ scripts/templates/mediaFactory.js
@@ -2,14 +2,16 @@
 export const mediaFactory = (media, thumbnail = false) => {
   let content;
   let src;
+  let path = `./assets/photographers/medias`;
+
   if (media.image) {
     content = document.createElement("img");
-    content.src = `./assets/photographers/medias/${media.photographerId}/${media.image}`;
+    content.src = `${path}/${media.photographerId}/${media.image}`;
     content.alt = media.title;
     content.className = "card-img picture";
   } else if (media.video) {
     content = document.createElement("video");
-    content.src = `./assets/photographers/medias/${media.photographerId}/${media.video}`;
+    content.src = `${path}/${media.photographerId}/${media.video}`;
     content.alt = media.title;
 
     if (!thumbnail) {
@@ -19,3 +21,18 @@ export const mediaFactory = (media, thumbnail = false) => {
   }
   return content;
 };
+
+// Voir plus tard (utiliser une class pour faire la media factories)
+// class mediaFactory {
+//   constructor(data) {
+//     if (data.image) {
+//       //   const img = document.createElement("img");
+//       return new img(data);
+//     } else if (data.video) {
+//       //   const media = document.createElement("video");
+//       return new media(data);
+//     } else {
+//       throw "Format de media non pris en charge";
+//     }
+//   }
+// }
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -7,3 +7,16 @@
 //     const modal = document.getElementById("contact_modal");
 //     modal.style.display = "none";
 // }
+const btnContact = document.querySelector(".contact_button");
+const modal = document.querySelector("#contact_modal");
+const closeModal = document.querySelector("#close_modal");
+const titlePhotograph = document.querySelector(".title_photograph");
+console.log(modal, btnContact);
+btnContact.addEventListener("click", () => {
+  modal.style.display = "block";
+  titlePhotograph.textContent =
+    document.querySelector(".photograph-title").textContent;
+});
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
